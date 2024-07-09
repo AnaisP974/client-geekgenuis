23aca896e1daef1cbce4965762257cbcab98c571
AnaisP974 <anaisperigny31@gmail.com>
2024-07-09 12:48:03 +0200

    décomposition du code

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Article } from "./interfaces/Article";
+import Card from "./Card";
 
 function Home() {
   // Récupérer tous les articles de db.json
@@ -11,28 +12,30 @@ function Home() {
   
   const [data, setData] = useState<Article[]>([]);
   
-  const getArticles = async() => {
-      try {
-          // const response = await fetch("/db.json")
-          const response = await fetch("/db.json")
-          const result = await response.json()
-          // je mets à jour mon state locale 
-          setData(result)
-          console.log(result);
-      }catch(error) {
-          // handle error
-          alert('Echec du fetch ')
+  useEffect(() => {
+      const getArticles = async() => {
+          try {
+              // const response = await fetch("/db.json")
+              const response = await fetch("/db.json")
+              const result = await response.json()
+              // je mets à jour mon state locale 
+              setData(result)
+              console.log(result);
+          }catch(error) {
+              // handle error
+              alert('Echec du fetch ')
+          }
       }
-  }
-    useEffect(() => {
         // je fetch ma data
         getArticles()
     }, []);
-    useEffect(() => {
-      console.log(data);
-      // Log data whenever it changes
-    }, [data]);
-  
+    // useEffect(() => {
+    //   console.log(data);
+    //   // Log data whenever it changes
+    // }, [data]);
+  // if(data.length === 0){
+  //   return <div>Loadingg...</div>
+  // }
   return (
     <>
       <section className="text-gray-400 bg-gray-900 body-font">
@@ -48,24 +51,9 @@ function Home() {
           </div>
         </div>
         <div className="flex flex-wrap -mx-4 -mt-4 -mb-10 sm:-m-4">
-          {data.map((article) => (
-          <article className="p-4 mb-6 md:w-1/3 sm:mb-0" key={article.id}>
-            <figure>
-              <div className="h-64 overflow-hidden rounded">
-                <img alt="content" className="object-cover object-center w-full h-full" src={article.img + `?random=` + article.id} />
-              </div>
-              <figcaption className="p-3 text-white bg-yellow-500">{article.category}</figcaption>
-            </figure>
-            <h2 className="mt-5 text-xl font-medium text-white title-font">{article.titre}</h2>
-            <p className="mt-2 text-base leading-relaxed">{article.desc}</p>
-            <p className="mt-2 text-base leading-relaxed"><i>{article.signature}, le {article.createdAt}</i> </p>
-            <a href={"/" + article.id} className="inline-flex items-center mt-3 text-indigo-400">Learn More
-              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-          </article>
-          ))}
+          {data.length>0 ? <Card articles={data} /> : "Loadingg..." }
+          
+          
          
         </div>
       </div>
