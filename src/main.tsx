a833eb7027114d597308e79fde0331ce66a697b0
AnaisP974 <anaisperigny31@gmail.com>
2024-07-09 14:53:16 +0200

    TailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home.tsx'
-import './style.css'
+import './output.css'
 import {
   createBrowserRouter,
   RouterProvider,
