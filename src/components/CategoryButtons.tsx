73bee1beb98562c7912cf58b35d239e7581c2c71
Anaïs PERIGNY <113293955+AnaisP974@users.noreply.github.com>
2024-07-11 16:32:30 +0200

    Update CategoryButtons.tsx

--- src/components/CategoryButtons.tsx
+++ src/components/CategoryButtons.tsx
@@ -10,10 +10,10 @@ interface CategoryButtonsProps {
 const CategoryButtons: React.FC<CategoryButtonsProps> = ({ categories, handleClick, catSelected }) => {
   return (
     <div className="flex items-center justify-center font-mono">
-      <div className="grid grid-cols-4 gap-4" id="buttons-cat">
+      <div className="grid grid-cols-4 gap-4"
         <h1 
           onClick={() => handleClick("All")} 
-          className={`px-3 py-1 shadow-lg shadow-gray-500/50 bg-gray-500 text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97] ${catSelected === "All" ? "bg-indigo-500" : ""}`}
+          className={`buttons-cat px-3 py-1 shadow-lg shadow-gray-500/50 bg-gray-500 text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97] ${catSelected === "All" ? "bg-indigo-500" : ""}`}
         >
           ALL
         </h1>
@@ -21,7 +21,7 @@ const CategoryButtons: React.FC<CategoryButtonsProps> = ({ categories, handleCli
           <h1 
             onClick={() => handleClick(category.name)} 
             key={category.id} 
-            className={`px-3 py-1 shadow-lg shadow-gray-500/50 bg-gray-500 text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97] ${catSelected === category.name ? "bg-indigo-500" : ""}`}
+            className={`buttons-cat px-3 py-1 shadow-lg shadow-gray-500/50 bg-gray-500 text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97] ${catSelected === category.name ? "bg-indigo-500" : ""}`}
           >
             {category.name}
           </h1>
