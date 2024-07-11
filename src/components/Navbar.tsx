6c6f4417e922568c3cddef46e8d2dcbcba64efab
AnaisP974 <anaisperigny31@gmail.com>
2024-07-11 16:54:55 +0200

    dark mode sur page détail

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,30 @@
 import { NavLink } from "react-router-dom"
+import { Toggle } from "./toggle"
+import { useState } from "react";
 
 const Navbar = () => {
+  
+  const [isDark, setIsdark] = useState(false);
+  const [mode, setMode] = useState('lune');
+  
+const handleChange = () => {
+  setIsdark(!isDark);
+  if (mode === 'lune') {
+    setMode('soleil');
+  } else {
+    setMode('lune');
+  }
+
+  document.body.classList.toggle('dark-mode');
+}
   return (
 
-    <nav className=" body-font flex justify-center pt-8">
+    <nav className="flex justify-center pt-8 body-font">
 
         <NavLink to={"/"}>
         <img src="./logo_geekgenuis.png" alt="" className="border w-36 border-slate-100" />
         </NavLink>
+        <Toggle handleChange={handleChange} isChecked={isDark} mode={mode}/>
     </nav>
   )
 }
